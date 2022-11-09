8df22ea1ca0f76aa8b56ce5d705c48d812666194
Himansumallick <dreamhimansu@gmail.com>
2022-11-09 11:59:43 +0545

    signin authenticated

--- apis/rooms.js
+++ apis/rooms.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
-
+require("dotenv").config();
+const express = require("express");
 const dbhandler = require("../databasehandler");
 const roomService = require("../databaseServices/room");
+const jwt = require("jsonwebtoken");
+router.use(express.json());
 
 router.get("/", async (req, res) => {
   try {
@@ -16,6 +19,39 @@ router.get("/", async (req, res) => {
       errorMessage: "An unexpected error occurred",
       data: [],
     });
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  try {
+    const data = req.body;
+    var id = data.userid;
+    var pass = data.password;
+
+    if (!id) {
+      throw new Error("UserId required");
+    }
+
+    if (!pass) {
+      throw new Error("Password required");
+    }
+
+    if (id === "galaxy" && pass === "12345") {
+      const user = { name: id, password: pass };
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+      res.json({ success: true, accessToken: accessToken });
+    } else {
+      throw new Error("Invalid Credentials");
+    }
+  } catch (error) {
+    return res.send({ success: false, message: error.message });
+  }
+});
+
+router.post("/localSignin/addRoom", async (req, res) => {
+  try {
+  } catch (error) {
+    return res.send({ success: false, message: error.message });
   }
 });
 
